469ceb04d3c55de44945fca06bc2f0ede62bbbc5
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2024-05-01 20:02:36 +0200

    added min height to hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,7 +6,7 @@ import DevImg from './DevImg'
 
 export default function Hero() {
 	return (
-		<section className='py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none'>
+		<section className='py-12 xl:py-24 min-h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none'>
 			<div className='container mx-auto'>
 				<div className='flex justify-between gap-x-8'>
 					<div className='flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left'>
