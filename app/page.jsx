f414bc292770b677793c21e62dfcf1e0571b5e7b
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2024-05-27 17:47:47 +0200

    added about page

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,16 @@
 import Hero from "@/components/Hero";
-import About from "@/components/About";
 import Services from "@/components/Services";
 import Work from "@/components/Work";
-import Reviews from "@/components/Reviews";
+// import Reviews from "@/components/Reviews";
 import Cta from "@/components/Cta";
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-y-40">
       <Hero />
-      <About />
       <Services />
       <Work />
-      <Reviews />
+      {/* <Reviews /> */}
       <Cta />
     </main>
   );
