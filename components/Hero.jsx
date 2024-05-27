13bc2de7fbcb5152e85b9109f40e9e6e297eb581
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2024-05-27 17:50:40 +0200

    added cv for download

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -19,15 +19,17 @@ export default function Hero() {
 									Contact me <Send size={18} />
 								</Button>
 							</Link>
-							<Button variant='secondary' className='gap-x-2'>
-								Download CV <Download size={18} />
-							</Button>
+							<Link href="/cv.pdf" target="_blank" rel="noopener noreferrer" download>
+								<Button variant='secondary' className='gap-x-2'>
+									Download CV <Download size={18} />
+								</Button>
+							</Link>
 						</div>
 						<Socials containerStyles='flex gap-x-6 mx-auto xl:mx-0' iconStyles='text-foreground text-[22px] hover:text-primary transition-all' />
 					</div>
 					<div className='hidden xl:flex relative'>
 						<div className='bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2'></div>
-						<DevImg containerStyles='bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom' imgSrc='/hero/developer.png' />
+						<DevImg containerStyles='bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom' imgSrc='/hero/profile.png' />
 					</div>
 				</div>
 			</div>
