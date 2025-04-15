f481cf958a480053607b2965c0b7f5a61379002d
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2025-04-15 17:33:52 +1200

    removed and added projects to projectData

--- components/Work.jsx
+++ components/Work.jsx
@@ -13,8 +13,16 @@ import ProjectCard from './ProjectCard';
 
 export const projectData = [
 	{
+		image: '/work/juvelia.png',
+		category: 'Client',
+		name: 'Juvelia.no',
+		description: "Shopify store redesign and SEO",
+		link: 'https://www.juvelia.no/',
+		github: 'https://github.com/EirikOrkelbog/ai-chat-app',
+	},
+	{
 		image: '/work/ai-chat.png',
-		category: 'typescript',
+		category: 'personal',
 		name: 'AI Chat App',
 		description: "I'm using OpenAI's API",
 		link: 'https://ai-chat-app-olive.vercel.app/',
@@ -22,31 +30,15 @@ export const projectData = [
 	},
 	{
 		image: '/work/guess-number.png',
-		category: 'javascript',
+		category: 'personal',
 		name: 'Guess My Number',
 		description: "Number guessing game",
 		link: 'https://guess-numberrrr-app.netlify.app/',
 		github: 'https://github.com/EirikOrkelbog/Guess-My-Number',
 	},
 	{
-		image: '/work/portfolio.png',
-		category: 'next.js',
-		name: 'Portfolio',
-		description: "My portfolio page",
-		link: '/',
-		github: 'https://github.com/EirikOrkelbog/eirikorkelbog-website',
-	},
-	{
-		image: '/work/dictionary-app.png',
-		category: 'javascript',
-		name: 'Dictionary App',
-		description: "Search for a word and find the meaning",
-		link: 'https://dictionaryyy-app.netlify.app/',
-		github: 'https://github.com/EirikOrkelbog/Dictionary-App',
-	},
-	{
 		image: '/work/movie-search.png',
-		category: 'javascript',
+		category: 'personal',
 		name: 'Movie search app',
 		description: "Get information about movies",
 		link: 'https://movie-search-apppp.netlify.app/',
@@ -54,23 +46,15 @@ export const projectData = [
 	},
 	{
 		image: '/work/recipe-app.png',
-		category: 'javascript',
+		category: 'personal',
 		name: 'Recipe App',
 		description: "Get recipe based on ingredient",
 		link: 'https://fooood-recipe-app.netlify.app/',
 		github: 'https://github.com/EirikOrkelbog/Food-Recipe-App',
 	},
 	{
-		image: '/work/resort-world.png',
-		category: 'javascript',
-		name: 'Resort World',
-		description: 'Recreation of Expedias page',
-		link: 'https://resort-world-concept.netlify.app/',
-		github: 'https://github.com/EirikOrkelbog/Resorts-World',
-	},
-	{
 		image: '/work/todo-list.png',
-		category: 'vue',
+		category: 'personal',
 		name: 'Todo List',
 		description: 'A simple todo list app with local storage',
 		link: 'https://addtaskapp.netlify.app/',
@@ -84,9 +68,6 @@ export default function Work() {
 			<div className='container mx-auto relative'>
 				<div className='max-w-[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start'>
 					<h2 className='section-title mb-8'>Latest Projects</h2>
-					<Link href='/projects'>
-						<Button>All projects</Button>
-					</Link>
 				</div>
 				{/* slider */}
 				<div className='xl:max-w-[1000px] xl:absolute right-0 top-0'>
