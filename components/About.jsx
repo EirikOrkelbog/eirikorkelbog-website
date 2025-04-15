f9fb7268d346f1bab2f582910adde1dbf3000970
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2025-04-15 17:30:52 +1200

    deleted education and experience data

--- components/About.jsx
+++ components/About.jsx
@@ -1,7 +1,8 @@
-import Image from 'next/image'
 import DevImg from './DevImg'
+import Link from 'next/link';
+import { Button } from "./ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from './ui/tabs'
-import { User2, MailIcon, PhoneIcon, HomeIcon, GraduationCap, Calendar, Briefcase } from 'lucide-react'
+import { User2, MailIcon, PhoneIcon, HomeIcon, Send } from 'lucide-react'
 
 const infoData = [
 	{
@@ -17,112 +18,14 @@ const infoData = [
 		text: 'eirikorkelbog@gmail.com',
 	},
 	{
-		icon: <Calendar size={20} />,
-		text: '03.04.1996',
-	},
-	{
-		icon: <GraduationCap size={20} />,
-		text: 'Kristiania University College',
-	},
-	{
 		icon: <HomeIcon size={20} />,
-		text: 'Øygarden, Norway',
+		text: 'Norway',
 	},
 ]
 
-const educationData = [
-	{
-		title: 'education',
-		data: [
-			{
-				university: 'Kristiania University College',
-				degree: 'Front-End Development',
-				years: '2022 - 2023',
-			},
-			{
-				university: 'Inland University College',
-				degree: 'Organization and Management',
-				years: '2021 - 2022',
-			},
-			{
-				university: 'University of Agder',
-				degree: 'Economy and Administration',
-				years: '2020 - 2021',
-			},
-			{
-				university: 'Inland University College',
-				degree: 'Economy and Administration',
-				years: '2018 - 2020',
-			},
-		],
-	},
+const experienceText1 = 'After starting out in web development, I transitioned into Shopify, where I found the perfect blend of tech, strategy, and creativity.'
 
-]
-
-const experienceData = [
-	{
-		title: 'experience',
-		data: [
-			{
-				company: 'Kodeverket',
-				role: 'Front-End Developer',
-				years: '2024 - 2024',
-			},
-			{
-				company: 'Generator Digital',
-				role: 'Marketing Assistant',
-				years: '2020 - 2021',
-			},
-			{
-				company: 'NG KIWI VEST AS',
-				role: 'Store employee',
-				years: '2019 - 2019',
-			},
-			{
-				company: 'NG KIWI VEST AS',
-				role: 'Store employee',
-				years: '2017 - 2018',
-			},
-		],
-	},
-]
-
-const skillData = [
-	{
-		title: 'skills',
-		data: [
-			{
-				name: 'HTML, CSS',
-			},
-			{
-				name: 'JavaScript',
-			},
-			{
-				name: 'React',
-			},
-			{
-				name: 'Next.js',
-			},
-			{
-				name: 'Git',
-			},
-			{
-				name: 'Sanity',
-			},
-		]
-	},
-	{
-		title: 'tools',
-		data: [
-			{
-				imgPath: '/about/figma.svg',
-			},
-			{
-				imgPath: '/about/vscode.svg',
-			},
-		]
-	},
-]
+const experienceText2 = 'Today, I specialize in Shopify theme development and Conversion Rate Optimization (CRO), helping brands build faster, smarter, and more profitable stores.'
 
 const About = () => {
 	const getData = (array, title) => {
@@ -134,14 +37,13 @@ const About = () => {
 				<h2 className='section-title mb-8 xl:mb-16 text-center mx-auto'>About me</h2>
 				<div className='flex flex-col xl:flex-row'>
 					<div className='hidden xl:flex flex-1 relative'>
-						<DevImg containerStyles='bg-about_shape_light dark:bg-about_shape_dark w-[505px] h-[505px] bg-no-repeat relative' imgSrc='/about/profile.png' />
+						<DevImg containerStyles='bg-about_shape_light dark:bg-about_shape_dark w-[505px] h-[505px] bg-no-repeat relative' imgSrc='/about/newprofile.png' />
 					</div>
 					<div className='flex-1'>
 						<Tabs defaultValue='personal'>
-							<TabsList className='w-full grid xl:grid-cols-3 xl:max-w-[520px] xl:border dark:border-none'>
+							<TabsList className='flex flex-col xl:flex-row xl:w-[300px] xl:border dark:border-none'>
 								<TabsTrigger className='w-[162px]' value='personal'>Personal</TabsTrigger>
 								<TabsTrigger className='w-[162px]' value='experience'>Experience</TabsTrigger>
-								<TabsTrigger className='w-[162px]' value='skills'>Skills</TabsTrigger>
 							</TabsList>
 							<div className='text-lg mt-12 xl:mt-8'>
 								<TabsContent value='personal'>
@@ -165,106 +67,18 @@ const About = () => {
 								<TabsContent value='experience'>
 									<div>
 										<h3 className='h3 mb-8 text-center xl:text-left'>My Journey</h3>
-										{/* wrapper */}
-										<div className='grid md:grid-cols-2 gap-y-8'>
-											{/* experience */}
-											<div className='flex flex-col gap-y-6'>
-												<div className='flex gap-x-4 items-center text-[22px] text-primary'>
-													<Briefcase />
-													<h4 className='capitalize font-medium'>
-														{getData(experienceData, 'experience').title}
-													</h4>
-												</div>
-												{/* list */}
-												<div className='flex flex-col gap-y-8'>
-													{getData(experienceData, 'experience').data.map((item, index) => {
-														const { company, role, years } = item;
-														return (
-															<div className='flex gap-x-8' key={index}>
-																<div className='h-[84px] w-[1px] bg-border relative ml-2'></div>
-																<div>
-																	<div className='font-semibold text-xl leading-none mb-2'>
-																		{company}
-																	</div>
-																	<div className='text-lg leading-none text-muted-foreground mb-4'>
-																		{role}
-																	</div>
-																	<div className='text-base font-medium'>
-																		{years}
-																	</div>
-																</div>
-															</div>
-														)
-													})}
-												</div>
-											</div>
-											{/* education */}
-											<div className='flex flex-col gap-y-6'>
-												<div className='flex gap-x-4 items-center text-[22px] text-primary'>
-													<GraduationCap size={28} />
-													<h4 className='capitalize font-medium'>
-														{getData(educationData, 'education').title}
-													</h4>
-												</div>
-												{/* list */}
-												<div className='flex flex-col gap-y-8'>
-													{getData(educationData, 'education').data.map((item, index) => {
-														const { university, degree, years } = item;
-														return (
-															<div className='flex gap-x-8' key={index}>
-																<div className='h-[84px] w-[1px] bg-border relative ml-2'></div>
-																<div>
-																	<div className='font-semibold text-xl leading-none mb-2'>
-																		{university}
-																	</div>
-																	<div className='text-lg leading-none text-muted-foreground mb-4'>
-																		{degree}
-																	</div>
-																	<div className='text-base font-medium'>
-																		{years}
-																	</div>
-																</div>
-															</div>
-														)
-													})}
-												</div>
-											</div>
-										</div>
-									</div>
-								</TabsContent>
-								<TabsContent value='skills'>
-									<div className='text-center xl:text-left'>
-										<h3 className='h3 mb-8'>What I Use Everyday</h3>
-										{/* skills */}
-										<div className='mb-16'>
-											<h4 className='text-xl font-semibold mb-2'>Skills</h4>
-											<div className='border-b border-border mb-4'></div>
-											{/* skill list */}
-											<div>
-												{getData(skillData, 'skills').data.map((item, index) => {
-													const { name } = item;
-													return (
-														<ul className='w-2/4 text-center xl:text-left mx-auto xl:mx-0' key={index}>
-															<li className='font-medium'>{name}</li>
-														</ul>
-													)
-												})}
-											</div>
-										</div>
-										{/* tools */}
-										<div>
-											<h4 className='text-xl font-semibold mb-2'>Tools</h4>
-											<div className='border-b border-border mb-4'></div>
-											<div className='flex gap-x-8 justify-center xl:justify-start'>
-												{getData(skillData, 'tools').data.map((item, index) => {
-													const { imgPath } = item;
-													return (
-														<div key={index}>
-															<Image src={imgPath} height={48} width={48} alt=''></Image>
-														</div>
-													)
-												})}
-											</div>
+										<div className='flex flex-col gap-4'>
+											<p className='text-lg leading-relaxed text-center xl:text-left'>
+												{experienceText1}
+											</p>
+											<p className='text-lg leading-relaxed text-center xl:text-left'>
+												{experienceText2}
+											</p>
+											<Link href='mailto:eirikorkelbog@gmail.com'>
+												<Button className='gap-x-2'>
+													Get in touch <Send size={18} />
+												</Button>
+											</Link>
 										</div>
 									</div>
 								</TabsContent>
