43189100d1db79bfa3ff166270869910c642ba2b
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2025-04-15 17:32:23 +1200

    updated text in hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -9,19 +9,14 @@ export default function Hero() {
 		<section className='py-12 xl:py-24 min-h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none'>
 			<div className='container mx-auto'>
 				<div className='flex justify-between gap-x-8'>
-					<div className='flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left'>
-						<span className='text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]'>Web Developer</span>
-						<h1 className='h1'>Hello, my name is Eirik Orkelbog</h1>
-						<p className='subtitle max-w-[490px] mx-auto xl:mx-0'>I take pleasure in crafting code that places a strong emphasis on design, accessibility, and strict adherence to industry best practices. I love to learn and improve my skills while building cool projects.</p>
+					<div className='flex max-w-[700px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left'>
+						<span className='text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]'>Shopify & CRO</span>
+						<h1 className='h1'>Stop losing sales to slow, clunky stores</h1>
+						<p className='subtitle max-w-[490px] mx-auto xl:mx-0'>Get a conversion-optimized Shopify store built for growth</p>
 						<div className='flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12'>
 							<Link href="mailto:eirikorkelbog@gmail.com">
 								<Button className='gap-x-2'>
 									Contact me <Send size={18} />
-								</Button>
-							</Link>
-							<Link href="/cv.pdf" target="_blank" rel="noopener noreferrer" download>
-								<Button variant='secondary' className='gap-x-2'>
-									Download CV <Download size={18} />
 								</Button>
 							</Link>
 						</div>
