480556b8443bc74da21b277a3cf89e59001b065e
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2025-04-15 17:27:43 +1200

    added services and cta

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,15 @@
 import Hero from "@/components/Hero";
-// import Services from "@/components/Services";
+import Services from "@/components/Services";
 import Work from "@/components/Work";
-// import Cta from "@/components/Cta";
+import Cta from "@/components/Cta";
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-y-40">
       <Hero />
-      {/* <Services /> */}
+      <Services />
       <Work />
-      {/* <Cta /> */}
+      <Cta />
     </main>
   );
 }
