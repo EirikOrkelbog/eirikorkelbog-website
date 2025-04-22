6b472b7e3e3ae857c93f3bc0d54684048f8e3e1a
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2025-04-22 16:48:39 +1200

    update

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,13 @@
 import Hero from "@/components/Hero";
-import Services from "@/components/Services";
-import Work from "@/components/Work";
 import Solution from "@/components/Solution";
-// import WhatsIncluded from "@/components/WhatsIncluded";
-import { Pricing } from "@/components/Pricing";
-import { About } from "@/components/About";
+import Pricing from "@/components/Pricing";
+import About from "@/components/About";
 import Cta from "@/components/Cta";
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-y-40">
       <Hero />
-      {/* <Services /> */}
-      {/* <Work /> */}
-      {/* <WhatsIncluded /> */}
       <Solution />
       <Pricing />
       <About />
