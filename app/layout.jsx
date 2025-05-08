ab7b992502278f9e63e2c9be335d25f3e15017e4
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2025-05-08 21:37:09 +1200

    updated metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,17 +6,17 @@ import Footer from "@/components/Footer";
 const open_sans = Open_Sans({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Eirik Orkelbog - Shopify & CRO",
-  description: "Shopify Store Optimization & CRO Specialist",
+  title: "Shopify Freelancer - Redesign + Structure Fixes That Convert",
+  description: "I help Shopify stores get a modern redesign and clean backend structure so customers convert better—and you stop wasting time fixing broken stuff.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={open_sans.className}>
-          <Header />
-          {children}
-          <Footer />
+        <Header />
+        {children}
+        <Footer />
       </body>
     </html>
   );
